699d12b777c5f95e9c16ebc75398ca12cb70f01d
Parth Shroff <pshroff@wpi.edu>
2026-04-18 03:18:41 -0700

    Inngest implementation

---
+++ src/app/(root)/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+<div className="flex flex-col items-center justify-center h-screen">
+  <Button>Test</Button>
+  <UserButton/>
+</div>
+  );
+}
